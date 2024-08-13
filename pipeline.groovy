ab82ead032a539a113d6bbd36f766f6614c932d4
psmurillom <psmurillom@indracompany.com>
2024-08-13 15:17:02 -0500

    Reuso pipeline

---
+++ pipeline.groovy
@@ -0,0 +1,16 @@
+def setupPipeline() {
+
+    stage('Build Docker Image') {
+        steps {
+            echo "Building Docker image..."
+        }
+    }
+
+    stage('Push Docker Image') {
+        steps {
+            echo "Pushing Docker image to registry..."
+        }
+    }
+
+    // Otros stages pueden ser agregados aquí
+}
