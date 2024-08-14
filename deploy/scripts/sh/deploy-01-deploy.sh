adb2198c7f3da64b61adbe720744ed63cfb52c95
psmurillom <psmurillom@indracompany.com>
2024-08-14 17:47:57 -0500

    Replicas

--- deploy/scripts/sh/deploy-01-deploy.sh
+++ deploy/scripts/sh/deploy-01-deploy.sh
@@ -3,11 +3,13 @@
 PROJECT=$APP_PROJECT_NAME
 IMAGE="${APP_DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}"
 
+
 awk -v project="$PROJECT" \
     -v image="$IMAGE" \
     -v profile="$APP_PROFILE" \
     -v port="$APP_PORT" \
     -v nodePort="$APP_NODEPORT" \
+    -v replicas="$APP_REPLICAS" \
     -v masterPass="$MASTER_PASS" '
 {
     gsub(/{{PROJECT}}/, project);
@@ -15,6 +17,7 @@ awk -v project="$PROJECT" \
     gsub(/{{PROFILE}}/, profile);
     gsub(/{{PORT}}/, port);
     gsub(/{{NODEPORT}}/, nodePort);
+    gsub(/{{APP_REPLICAS}}/, replicas);
     gsub(/{{MASTER_PASS}}/, masterPass);
     print;
 }' deploy/deployment.yaml > deployment.yaml
