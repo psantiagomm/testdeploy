4caacce58244730bf07b6b81ecd54174ce6ba6f1
psmurillom <psmurillom@indracompany.com>
2024-08-14 17:16:04 -0500

    Docker registry

--- deploy/scripts/groovy/deployStages.groovy
+++ deploy/scripts/groovy/deployStages.groovy
@@ -1,8 +1,8 @@
 def buildStage() {
     defineEnvVars()
     echo "Building the application... APP_PROJECT_NAME=${env.APP_PROJECT_NAME}"
-    echo "La imagen que se va a generar es: ${DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}"
-    docker.build("${DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}", "-f $APP_DOCKERFILE $APP_DOCKER_CONTEXT")
+    echo "La imagen que se va a generar es: ${APP_DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}"
+    docker.build("${APP_DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}", "-f $APP_DOCKERFILE $APP_DOCKER_CONTEXT")
     echo "Imagen contruida exitosamente"
     echo "Funciona env.APP_FUNCIONA ${env.APP_FUNCIONA}"
     echo "Funciona APP_FUNCIONA ${APP_FUNCIONA}"
@@ -10,8 +10,8 @@ def buildStage() {
 
 def pushStage() {
     defineEnvVars()
-    docker.withRegistry("http://${DOCKER_REGISTRY}") {
-        docker.image("${DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}").push()
+    docker.withRegistry("http://${APP_DOCKER_REGISTRY}") {
+        docker.image("${APP_DOCKER_REGISTRY}/${APP_IMAGE_FULL_NAME}").push()
     }
 }
 
