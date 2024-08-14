027d67109680ea002a2c67171e44f9679a14e53c
psmurillom <psmurillom@indracompany.com>
2024-08-14 11:28:20 -0500

    Namespace

--- deploy/scripts/sh/update-properties-01-configmap.sh
+++ deploy/scripts/sh/update-properties-01-configmap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Aplicar el ConfigMap en Minikube
-kubectl apply -f configmap.yaml --context="$APP_KUBE_CONTEXT"
+kubectl apply -f configmap.yaml -n "$APP_KUBE_CONTEXT"
 
 rm configmap.yaml
