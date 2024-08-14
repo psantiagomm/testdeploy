e3956d73bbc1843a7bf8f9544dfde09601fd7d26
psmurillom <psmurillom@indracompany.com>
2024-08-14 17:35:50 -0500

    Ajustes

--- deploy/scripts/groovy/deployStages.groovy
+++ deploy/scripts/groovy/deployStages.groovy
@@ -29,6 +29,16 @@ def deployStage() {
 def defineEnvVars() {
     env.IMAGE_NAME = env.APP_PROJECT_NAME
     env.APP_FUNCIONA = "Valor de funciona"
+
+    def pom = readMavenPom file: "$APP_POM_PATH"
+    env.APP_ARTIFACT_ID = pom.artifactId
+    env.APP_ARTIFACT_VERSION = pom.version
+    env.APP_IMAGE_FULL_NAME = "${ARTIFACT_ID}:${ARTIFACT_VERSION}"
+
+
+    echo "POM Content: ${pom}"
+    echo "ArtifactId: ${pom.artifactId ?: 'No ArtifactId found'}"
+    echo "Version: ${pom.version ?: 'No Version found'}"
 }
 
 return this
