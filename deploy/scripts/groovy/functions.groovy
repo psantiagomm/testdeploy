85b9d3c4f52e1808961d975df6127aff745528b8
psmurillom <psmurillom@indracompany.com>
2024-08-16 14:17:52 -0500

    Ajuste paths personalizables

--- deploy/scripts/groovy/functions.groovy
+++ deploy/scripts/groovy/functions.groovy
@@ -3,6 +3,7 @@ def defineDefaultVars() {
     echo "Default environment variables"
     env.APP_DOCKER_REGISTRY = "${env.APP_DOCKER_REGISTRY ?: 'localhost:5000'}"
     env.APP_SCRIPTS_PATH = "${env.APP_SCRIPTS_PATH ?: './deploy/scripts'}"
+
     env.APP_SH_SCRIPTS_DEPLOY_PATH = defineValue(env.APP_SH_SCRIPTS_DEPLOY_PATH, "${env.APP_SCRIPTS_PATH}/sh/deploy-01-deploy.sh")
 }
 
@@ -17,6 +18,18 @@ def defineImage() {
     echo "ArtifactId: ${pom.artifactId ?: 'No ArtifactId found'}"
     echo "Version: ${pom.version ?: 'No Version found'}"
 }
+
+def defineDefaultVarsUpdateProperties() {
+    // Default environment variables
+    echo "Default environment variables"
+    env.APP_DOCKER_REGISTRY = "${env.APP_DOCKER_REGISTRY ?: 'localhost:5000'}"
+    env.APP_SCRIPTS_PATH = "${env.APP_SCRIPTS_PATH ?: './deploy/scripts'}"
+
+    env.APP_SH_SCRIPTS_PROPERTIES_CONFIGMAP_PATH = defineValue(env.APP_SH_SCRIPTS_PROPERTIES_CONFIGMAP_PATH, "${env.APP_SCRIPTS_PATH}/sh/update-properties-01-configmap.sh")
+    env.APP_SH_SCRIPTS_PROPERTIES_WAIT_PATH = defineValue(env.APP_SH_SCRIPTS_PROPERTIES_WAIT_PATH, "${env.APP_SCRIPTS_PATH}/sh/update-properties-02-wait.sh")
+    env.APP_SH_SCRIPTS_PROPERTIES_REFRESH_PATH = defineValue(env.APP_SH_SCRIPTS_PROPERTIES_REFRESH_PATH, "${env.APP_SCRIPTS_PATH}/sh/update-properties-03-refresh-pods.sh")
+}
+
 
 def defineValue(value, defaultValue) {
     return "${value ?: defaultValue}"
--- deploy/scripts/groovy/updatePropertiesStages.groovy
+++ deploy/scripts/groovy/updatePropertiesStages.groovy
@@ -1,23 +1,34 @@
 def updateConfigmapStage() {
+    defineEnvVars()
     withCredentials([string(credentialsId: "$APP_MASTER_PASS_CREDENTIAL_ID", variable: 'MASTER_PASS')]) {
     sh '''
         #!/bin/bash
         chmod +x ${APP_CREATE_CONFIGMAP_SCRIPT}
-        chmod +x ./deploy/scripts/sh/update-properties-01-configmap.sh
+        chmod +x ${APP_SH_SCRIPTS_PROPERTIES_CONFIGMAP_PATH}
         ${APP_CREATE_CONFIGMAP_SCRIPT}
-        ./deploy/scripts/sh/update-properties-01-configmap.sh
+        ${APP_SH_SCRIPTS_PROPERTIES_CONFIGMAP_PATH}
     '''
     }
 }
 
 def waitStage() {
-    sh 'chmod +x ./deploy/scripts/sh/update-properties-02-wait.sh'
-    sh './deploy/scripts/sh/update-properties-02-wait.sh'
+    defineEnvVars()
+    sh 'chmod +x ${APP_SH_SCRIPTS_PROPERTIES_WAIT_PATH}'
+    sh '${APP_SH_SCRIPTS_PROPERTIES_WAIT_PATH}'
 }
 
 def refreshPodsStage() {
-    sh 'chmod +x ./deploy/scripts/sh/update-properties-03-refresh-pods.sh'
-    sh './deploy/scripts/sh/update-properties-03-refresh-pods.sh'
+    defineEnvVars()
+    sh 'chmod +x ${APP_SH_SCRIPTS_PROPERTIES_REFRESH_PATH}'
+    sh '${APP_SH_SCRIPTS_PROPERTIES_REFRESH_PATH}'
+}
+
+def defineEnvVars() {
+    env.APP_SCRIPTS_PATH = "${env.APP_SCRIPTS_PATH ?: './deploy/scripts'}"
+    env.APP_GROOVY_SCRIPTS_FUNCTIONS_PATH = defineValue(env.APP_GROOVY_SCRIPTS_FUNCTIONS_PATH, "${env.APP_SCRIPTS_PATH}/groovy/functions.groovy")
+
+    def functions = load "${APP_GROOVY_SCRIPTS_FUNCTIONS_PATH}"
+    functions.defineDefaultVarsUpdateProperties()
 }
 
 return this
